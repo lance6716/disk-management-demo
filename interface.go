459715e6cfb49c3a175fbd1aa342d2195f3244d1
lance6716 <lance6716@gmail.com>
2024-02-20 20:54:45 +0800

    add more variables and comments
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- interface.go
+++ interface.go
@@ -1,10 +1,24 @@
 package disk_management_demo
 
-type SpaceHandle struct{}
+import "errors"
+
+type SpaceHandle struct {
+	// private field
+	// TODO
+}
+
+var (
+	ErrDiskFull      = errors.New("disk is full")
+	ErrInvalidHandle = errors.New("invalid handle")
+)
 
 type Manager interface {
 	// Write persists the data to the storage and returns a handle of it.
+	//
+	// If the storage is full, it returns ErrDiskFull.
 	Write(data []byte) (SpaceHandle, error)
 	// Delete removes the data of given handle from the storage.
+	//
+	// If the handle does not exist, it returns ErrInvalidHandle.
 	Delete(handle SpaceHandle) error
 }
